258922a512a17fdd02cf522d6d286ed669348ccb
Wessam-Ahmed <wessam.ah@outlook.com>
2023-06-19 17:34:04 +0400

    Stack should log to one channel

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'daily'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
         ],
 
